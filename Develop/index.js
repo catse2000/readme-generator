29b01b43ec609fbc56a94fcefdd704abf1df9b4a
catse2000 <catse2000@yahoo.com>
2020-09-27 20:56:05 -0500

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,63 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+// const fs = require('fs');
+// const generateReadMe = require('./src/readme-template.js');
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your Project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a name for your Project!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: "What is the description for your project?"
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What are the instructions for installation?'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What are your usage instructions?'
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'What license is this project under? (check all that apply)',
+            choices: ['PD', 'CC0', 'MIT', 'Apache', 'MPL', 'GPL', 'AGPL', 'JRL', 'AFPL', 'Private', 'None']
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'What are your contribution guidelines?'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What tests have you done?'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'What questions would you like to add?'
+        }
+    ])
+    .then(answers => console.log(answers));
+};
 
-// function to initialize program
-function init() {
 
-}
-
-// function call to initialize program
-init();
+promptUser();
